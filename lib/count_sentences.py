a095a5305293a702a505a0599d6a523099c6d214
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-03-29 14:31:25 -0400

    Getting trickier. the counting sentences required the use of regex to avoid a serious headache signaling more familiarity with this going forward. The problems are getting more creative such that empty elements in an array should be removed from the returned value of a func

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+  
+  def __init__(self, value=""):
+    self.value = value
+    
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, string):
+    self._value=string if type(string) == str else print("The value must be a string.")
+    
+  
+  def is_sentence(self):
+    return True if self._value[-1] == "." else False
+
+  def is_exclamation(self):
+    return True if self._value[-1] == "!" else False
+  
+  def is_question(self):
+    return True if self._value[-1] == "?" else False
+  
+  def count_sentences(self):
+    results = re.split(r'[?.!]', self._value)
+    return len([element for element in results if element])
+
+if __name__ == "__main__":
+  stringy = MyString("one. two. three?")
+  # stringy = MyString(33)
+  print(stringy.count_sentences())
